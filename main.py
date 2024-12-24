b570e6f90cccf8e460a8fd3083d4be088b5625a9
Younes Idrissi <younesemail0@gmail.com>
2024-12-24 12:20:08 -0500

    further gui updates

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ layers.columnconfigure(0, weight=1)#one column, three rows
 img = tk.Label(layers, text="Image goes here", font=("Ariel", 30))
 img.grid(row=0)
 
+temp = tk.Label(layers, text=f"72 F - Sunny", font=("Ariel", 30))
+temp.grid(row=1)
+
+location = tk.Label(layers, text="Philadelphia, PA", font=("Ariel", 30))
+location.grid(row=2)
+
 layers.pack()
 
 
